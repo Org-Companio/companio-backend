85b997660818e973acc99508fe722f17c42f3523
hisreal123 <omosgoerge@gmail.com>
2025-12-15 11:17:11 +0100

    model

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from djano.contrib.auth.models import AbstractUser
+
+class User (AbstractUser):
+  ROLE_CHOICE = (
+    ('BOOKER', 'booker'),
+    ('COMPANION', 'companion')
+  )
+  phone = models.CharField(max_length=20, unique=True)
+  email = models.CharField(max_length=20, unique=True)
+  role = models.CharField(max_length=20, choice=ROLE_CHOICE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True) # updates the field evrytime sjango saves.
